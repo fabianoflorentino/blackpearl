829cf7d7a724c22183d5a64a7ddb938abe85616c
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-03-02 14:57:54 -0300

    Fix typo in transaction table column name

--- db/migrate/20240302151936_create_transactions.rb
+++ db/migrate/20240302151936_create_transactions.rb
@@ -6,7 +6,7 @@ class CreateTransactions < ActiveRecord::Migration[7.1]
     create_table :transactions do |t|
       t.integer :amount, null: false, default: 0
       t.string :type, null: false
-      t.text :desciption, null: false, limit: 10
+      t.text :description, null: false, limit: 10
 
       t.timestamps
     end
