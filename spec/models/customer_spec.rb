9f3a6ce7845af4301777d7c2210f8ebeab98c847
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-03-02 16:10:18 -0300

    Refactor customer_spec.rb to use is_expected syntax

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -3,35 +3,35 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  let(:customer) { Customer.new }
+  let(:customer) { described_class.new }
 
-  it { should have_many(:transactions) }
-  it { should validate_presence_of(:name) }
-  it { should validate_presence_of(:limit) }
-  it { should validate_presence_of(:balance) }
+  it { is_expected.to have_many(:transactions) }
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of(:limit) }
+  it { is_expected.to validate_presence_of(:balance) }
 
-  it 'should validate the presence of the name' do
+  it 'validates the presence of the name' do
     customer.name = nil
 
     customer.valid?
     expect(customer.errors[:name]).to include("can't be blank")
   end
 
-  it 'should validate the presence of the limit' do
+  it 'validates limit is greater than 0' do
     customer.limit = -1
 
     customer.valid?
     expect(customer.errors[:limit]).to include('must be greater than 0')
   end
 
-  it 'should validate the presence of the limit' do
+  it 'validates limit is not a string' do
     customer.limit = 'string'
 
     customer.valid?
     expect(customer.errors[:limit]).to include('is not a number')
   end
 
-  it 'should validate the presence of the balance' do
+  it 'validates balance is not a string' do
     customer.balance = 'string'
 
     customer.valid?
