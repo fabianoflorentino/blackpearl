7de41d12c54da3af2b572d0b0c2fa83221d41d7e
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-03-02 16:10:25 -0300

    Refactor transaction_spec.rb to use is_expected.to syntax

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -3,36 +3,36 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  let(:transaction) { Transaction.new }
+  let(:transaction) { described_class.new }
 
-  it { should belong_to(:customer) }
-  it { should validate_presence_of(:amount) }
-  it { should validate_numericality_of(:amount).is_greater_than(0) }
-  it { should validate_presence_of(:type) }
-  it { should validate_inclusion_of(:type).in_array(%w[c d]) }
-  it { should validate_presence_of(:description) }
+  it { is_expected.to belong_to(:customer) }
+  it { is_expected.to validate_presence_of(:amount) }
+  it { is_expected.to validate_numericality_of(:amount).is_greater_than(0) }
+  it { is_expected.to validate_presence_of(:type) }
+  it { is_expected.to validate_inclusion_of(:type).in_array(%w[c d]) }
+  it { is_expected.to validate_presence_of(:description) }
 
-  it 'should validate the presence of the customer' do
+  it 'validates the presence of the customer' do
     transaction.valid?
     expect(transaction.errors[:customer]).to include('must exist')
   end
 
-  it 'should validate the presence of the amount' do
+  it 'validates the presence of the amount' do
     transaction.valid?
     expect(transaction.errors[:amount]).to include('must be greater than 0')
   end
 
-  it 'should validate the presence of the type' do
+  it 'validates the presence of the type' do
     transaction.valid?
     expect(transaction.errors[:type]).to include('is not included in the list')
   end
 
-  it 'should validate the presence of the description' do
+  it 'validates the presence of the description' do
     transaction.valid?
     expect(transaction.errors[:description]).to include("can't be blank")
   end
 
-  it 'should validate the description length' do
+  it 'validates the description length' do
     transaction.description = 'a' * 11
 
     transaction.valid?
