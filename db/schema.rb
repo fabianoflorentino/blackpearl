31316a6e27587ab7d2e003555bcdcfdc67c3ac21
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-03-02 00:21:12 -0300

    Add customers table to schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 20_240_302_015_845) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
+  create_table 'customers', force: :cascade do |t|
+    t.string 'name'
+    t.integer 'limit', default: 0
+    t.integer 'balance', default: 0
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['balance'], name: 'index_customers_on_balance'
+  end
 end
