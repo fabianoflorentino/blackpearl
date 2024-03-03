b0b1937adeeadd9fa71d9ce9f04b4d15acb50f85
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-03-03 18:32:05 -0300

    Add error handling in ApplicationController and remove customers_spec.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 # frozen_string_literal: true
 
+# ApplicationController
 class ApplicationController < ActionController::API
+  rescue_from StandardError, with: :name_in_use
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  private
+
+  def name_in_use(exception)
+    render json: { error: exception.message }, status: :unprocessable_entity
+  end
+
+  def record_not_found(exception)
+    render json: { error: exception.message }, status: :not_found
+  end
 end
