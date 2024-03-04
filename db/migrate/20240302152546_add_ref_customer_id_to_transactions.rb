53194822ccc382550c1d37fdd870ad9992228ef3
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-03-04 12:50:01 -0300

    Update customer reference in transactions migration

--- db/migrate/20240302152546_add_ref_customer_id_to_transactions.rb
+++ db/migrate/20240302152546_add_ref_customer_id_to_transactions.rb
@@ -3,10 +3,10 @@
 # Transaction model which is used to interact with the database.
 class AddRefCustomerIdToTransactions < ActiveRecord::Migration[7.1]
   def up
-    add_reference :transactions, :customer, null: false, default: 1, foreign_key: true
+    add_reference :transactions, :customers, null: false, default: 1, foreign_key: true
   end
 
   def down
-    remove_reference :transactions, :customer
+    remove_reference :transactions, :customers
   end
 end
