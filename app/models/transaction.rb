4d2e83ea9bf0c6dea202b3378fa1ceaafa001c48
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-03-05 14:01:34 -0300

    Add validation for special characters in description field

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -7,4 +7,14 @@ class Transaction < ApplicationRecord
   validates :amount, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :kind, presence: true, inclusion: { in: %w[c d] }
   validates :description, presence: true, length: { maximum: 10 }
+
+  validate :handle_description_characters_special
+
+  private
+
+  def handle_description_characters_special
+    return unless /[^a-zA-Z0-9\s]/.match?(description)
+
+    errors.add(:description, 'should not contain special characters')
+  end
 end
