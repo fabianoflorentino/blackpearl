94b23745ed9e13f9e2b327848d8e1d435348ad5e
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-03-07 00:03:27 -0300

    Add ExtractsController with index action

---
+++ app/controllers/extracts_controller.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# Extract controller
+class ExtractsController < ApplicationController
+  def index
+    extract ||= TransactionUseCase::Extract.new(params[:customer_id]).call
+
+    render json: extract, status: :ok
+  end
+end
