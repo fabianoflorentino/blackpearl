b3a07004906d3dcd6bae1457cc4853b235cae583
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-03-08 15:19:10 -0300

    Add exception handling for NameInUse, StatementInvalid, and RecordInvalid

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,10 @@
 
 # ApplicationController
 class ApplicationController < ActionController::API
-  rescue_from StandardError, with: :name_in_use
+  rescue_from SharedErrors::NameInUse, with: :name_in_use
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ActiveRecord::StatementInvalid, with: :customer_limit_reached
+  rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
 
   private
 
@@ -13,5 +15,15 @@ class ApplicationController < ActionController::API
 
   def record_not_found(exception)
     render json: { error: exception.message }, status: :not_found
+  end
+
+  def customer_limit_reached(exception)
+    render json: { error: 'Customer limit reached' }, status: :unprocessable_entity
+
+    Rails.logger.error(exception.message)
+  end
+
+  def record_invalid(exception)
+    render json: { error: exception.message }, status: :unprocessable_entity
   end
 end
