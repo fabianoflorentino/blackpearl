f34ea050d8fb542255cac2049d61dace9f0b459e
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-03-10 01:14:30 -0300

    Remove unused rescue_from method in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,11 @@
 
 # ApplicationController
 class ApplicationController < ActionController::API
-  rescue_from SharedErrors::NameInUse, with: :name_in_use
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   rescue_from ActiveRecord::StatementInvalid, with: :customer_limit_reached
   rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
 
   private
-
-  def name_in_use(exception)
-    render json: { error: exception.message }, status: :unprocessable_entity
-  end
 
   def record_not_found(exception)
     render json: { error: exception.message }, status: :not_found
