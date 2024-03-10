bc013f7260ede27a14f25696d34b78a56433026e
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-03-10 01:29:11 -0300

    Update schema.rb with new columns for customers table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_07_184148) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_10_024833) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,7 +20,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_07_184148) do
     t.integer "balance", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
+    t.string "password"
     t.index ["balance"], name: "index_customers_on_balance"
+    t.index ["email"], name: "index_customers_on_email", unique: true
+    t.index ["name"], name: "index_customers_on_name", unique: true
   end
 
   create_table "transactions", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
