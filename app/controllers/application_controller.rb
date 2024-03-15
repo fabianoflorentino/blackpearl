3d5c8de54bf7736f0a96823c571d50e14395e632
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-03-15 12:19:35 -0300

    Add error handling for custom exceptions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,10 @@
 # ApplicationController
 class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
-  rescue_from ActiveRecord::StatementInvalid, with: :customer_limit_reached
   rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
+  rescue_from SharedErrors::LimitReached, with: :customer_limit_reached
+  rescue_from SharedErrors::CustomerNotFound, with: :customer_not_found
+  rescue_from SharedErrors::WrongPassword, with: :wrong_password
 
   private
 
@@ -13,12 +15,20 @@ class ApplicationController < ActionController::API
   end
 
   def customer_limit_reached(exception)
-    render json: { error: 'Customer limit reached' }, status: :unprocessable_entity
+    render json: { error: exception.message }, status: :unprocessable_entity
 
     Rails.logger.error(exception.message)
   end
 
   def record_invalid(exception)
+    render json: { error: exception.message }, status: :unprocessable_entity
+  end
+
+  def customer_not_found(exception)
+    render json: { error: exception.message }, status: :not_found
+  end
+
+  def wrong_password(exception)
     render json: { error: exception.message }, status: :unprocessable_entity
   end
 end
