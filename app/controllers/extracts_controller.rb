070a70df8bbb228cb87c2ba0eb56fb81c22e878a
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-03-17 20:42:50 -0300

    Add authorization before extracting data

--- app/controllers/extracts_controller.rb
+++ app/controllers/extracts_controller.rb
@@ -2,9 +2,16 @@
 
 # Extract controller
 class ExtractsController < ApplicationController
+  before_action :authorize_request
+
   def index
     extract = TransactionUseCase::Extract.new(params[:customer_id]).call
-
     render json: extract, status: :ok
+  end
+
+  private
+
+  def authorize_request
+    AuthenticationUseCase::Authorize.new(request.headers).call
   end
 end
