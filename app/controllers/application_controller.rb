06fe1f72506784411e6304db51518f609a7553d7
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-03-16 21:43:21 -0300

    Update rescue_from exception in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@
 class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
-  rescue_from SharedErrors::LimitReached, with: :customer_limit_reached
+  rescue_from ActiveRecord::StatementInvalid, with: :customer_limit_reached
   rescue_from SharedErrors::CustomerNotFound, with: :customer_not_found
   rescue_from SharedErrors::WrongPassword, with: :wrong_password
 
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::API
   end
 
   def customer_limit_reached(exception)
-    render json: { error: exception.message }, status: :unprocessable_entity
+    render json: { error: 'Customer limit reached' }, status: :unprocessable_entity
 
     Rails.logger.error(exception.message)
   end
