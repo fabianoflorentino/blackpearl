19f4c72c1956abb5343219c0a7322b8f133fd4f6
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-03-17 00:37:16 -0300

    Update schema.rb with new index on customer_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_10_024833) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_17_021208) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,6 +35,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_10_024833) do
     t.datetime "updated_at", null: false
     t.uuid "customer_id"
     t.index ["amount"], name: "index_transactions_on_amount"
+    t.index ["customer_id"], name: "index_transactions_on_customer_id"
   end
 
   add_foreign_key "transactions", "customers"
