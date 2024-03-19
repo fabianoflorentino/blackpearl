5d8acbd03cabc79fddda093fa5968d7049544268
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-03-18 22:15:36 -0300

    Add authorization headers to transaction create requests

--- spec/requests/transactions/create_spec.rb
+++ spec/requests/transactions/create_spec.rb
@@ -5,11 +5,13 @@ require 'rails_helper'
 RSpec.describe 'POST - /customers/:id/transactions' do
   let(:customer) { create(:customer) }
   let(:transaction) { attributes_for(:transaction) }
+  let(:authorization) { AuthenticationUseCase::Token.new(customer.email, customer.password).call }
+  let(:headers) { { 'Authorization' => "Bearer #{authorization}" } }
   let(:url) { "/customers/#{customer.id}/transactions" }
 
   context 'when the request is valid' do
     it 'returns status code 201' do
-      post(url, params: { transaction: })
+      post(url, headers:, params: { transaction: })
 
       expect(response).to have_http_status(:created)
     end
@@ -17,25 +19,25 @@ RSpec.describe 'POST - /customers/:id/transactions' do
 
   context 'when the request is invalid' do
     it 'returns status code 422 if amount is invalid' do
-      post(url, params: { transaction: { amount: nil } })
+      post(url, headers:, params: { transaction: { amount: nil } })
 
       expect(response).to have_http_status(:unprocessable_entity)
     end
 
     it 'returns status code 422 if amount is less than 0' do
-      post(url, params: { transaction: { amount: -1 } })
+      post(url, headers:, params: { transaction: { amount: -1 } })
 
       expect(response).to have_http_status(:unprocessable_entity)
     end
 
     it 'returns status code 422 if amount is not integer' do
-      post(url, params: { transaction: { amount: 1.1 } })
+      post(url, headers:, params: { transaction: { amount: 1.1 } })
 
       expect(response).to have_http_status(:unprocessable_entity)
     end
 
     it 'returns status code 422 if kind is invalid' do
-      post(url, params: { transaction: { kind: 'x' } })
+      post(url, headers:, params: { transaction: { kind: 'x' } })
 
       expect(response).to have_http_status(:unprocessable_entity)
     end
@@ -43,7 +45,7 @@ RSpec.describe 'POST - /customers/:id/transactions' do
     it 'returns status code 422 if description is invalid' do
       description = Faker::Lorem.characters(number: 11)
 
-      post(url, params: { transaction: { description: } })
+      post(url, headers:, params: { transaction: { description: } })
 
       expect(response).to have_http_status(:unprocessable_entity)
     end
@@ -51,7 +53,7 @@ RSpec.describe 'POST - /customers/:id/transactions' do
     it 'returns status code 422 if description has special characters' do
       description = '!@#$%^&**()[]_+'
 
-      post(url, params: { transaction: { description: } })
+      post(url, headers:, params: { transaction: { description: } })
 
       expect(response).to have_http_status(:unprocessable_entity)
     end
@@ -59,7 +61,7 @@ RSpec.describe 'POST - /customers/:id/transactions' do
     it 'returns status code 422 if description is nil' do
       description = nil
 
-      post(url, params: { transaction: { description: } })
+      post(url, headers:, params: { transaction: { description: } })
 
       expect(response).to have_http_status(:unprocessable_entity)
     end
@@ -67,9 +69,12 @@ RSpec.describe 'POST - /customers/:id/transactions' do
     it 'returns status code 422 if customer_id is invalid' do
       customer_id = SecureRandom.uuid
 
-      post("/customers/#{customer_id}/transactions", params: { transaction: })
+      url = "/customers/#{customer_id}/transactions"
 
-      expect(response).to have_http_status(:unprocessable_entity)
+      post(url, headers: { 'Authorization' => 'invalid_token' }, params: { transaction: })
+
+      expect(response).to have_http_status(:unauthorized)
+      expect(response.parsed_body['error']).to eq('Unauthorized request')
     end
   end
 end
