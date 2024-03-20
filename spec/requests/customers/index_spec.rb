0c48524a3bea9ce2e7ea6303b475be8fbb70457d
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-03-19 23:53:15 -0300

    Add authorization headers to customer index and show requests

--- spec/requests/customers/index_spec.rb
+++ spec/requests/customers/index_spec.rb
@@ -8,8 +8,10 @@ RSpec.describe 'GET - /customers' do
   context 'when there are customers' do
     it 'returns a list of customers' do
       customers = create_list(:customer, 10)
+      authorization = AuthenticationUseCase::Token.new(customers.first.email, customers.first.password).call
+      headers = { 'Authorization' => "Bearer #{authorization}" }
 
-      get(url)
+      get(url, headers:)
 
       response_body = response.parsed_body['customers']
 
@@ -20,8 +22,10 @@ RSpec.describe 'GET - /customers' do
 
     it 'returns a single customer' do
       customer = create(:customer)
+      authorization = AuthenticationUseCase::Token.new(customer.email, customer.password).call
+      headers = { 'Authorization' => "Bearer #{authorization}" }
 
-      get("#{url}/#{customer.id}")
+      get("#{url}/#{customer.id}", headers:)
 
       response_body = response.parsed_body['customer']
 
@@ -31,8 +35,10 @@ RSpec.describe 'GET - /customers' do
 
     it 'returns attributes for a single customer' do
       customer = create(:customer)
+      authorization = AuthenticationUseCase::Token.new(customer.email, customer.password).call
+      headers = { 'Authorization' => "Bearer #{authorization}" }
 
-      get("#{url}/#{customer.id}")
+      get("#{url}/#{customer.id}", headers:)
 
       response_body = response.parsed_body['customer']
 
@@ -42,28 +48,38 @@ RSpec.describe 'GET - /customers' do
       expect(response_body['limit']).to eq(customer.limit)
       expect(response_body['balance']).to eq(customer.balance)
     end
+
+    it 'returns only one customer' do
+      customers = create(:customer)
+      authorization = AuthenticationUseCase::Token.new(customers.email, customers.password).call
+      headers = { 'Authorization' => "Bearer #{authorization}" }
+
+      get(url, headers:)
+
+      response_body = response.parsed_body['customers']
+
+      expect(response).to have_http_status(:ok)
+      expect(response_body.count).to eq(1)
+    end
   end
 
   context 'when there are no customers' do
-    it 'returns an empty list' do
-      get(url)
-
-      expect(response).to have_http_status(:ok)
-      expect(response.body).to eq('{"customers":[]}')
-    end
-
-    it 'returns a 404' do
+    it 'returns a 401' do
       get("#{url}/1")
 
-      expect(response).to have_http_status(:not_found)
+      expect(response).to have_http_status(:unauthorized)
     end
   end
 
   context 'when there is an error' do
     it 'returns a 500' do
+      customer = create(:customer)
+      authorization = AuthenticationUseCase::Token.new(customer.email, customer.password).call
+      headers = { 'Authorization' => "Bearer #{authorization}" }
+
       allow(Customer).to receive(:all).and_raise(StandardError)
 
-      get(url)
+      get(url, headers:)
 
       expect(response).to have_http_status(:server_error)
     end
