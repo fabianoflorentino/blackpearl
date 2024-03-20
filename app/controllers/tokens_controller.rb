b12f136d4bd41b5725315dbc3361b38290fcfc5a
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-03-19 23:52:44 -0300

    Remove unnecessary line in TokensController create method

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -4,7 +4,6 @@
 class TokensController < ApplicationController
   def create
     token = AuthenticationUseCase::Token.new(params[:email], params[:password]).call
-
     render json: { token: }, status: :created
   end
 end
