72baf99f05637c54cc41ac45b4ac80a75ba9d378
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-03-31 12:55:43 -0300

    Update schema.rb with new version and enable pgcrypto extension

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_29_165141) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_31_143457) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
   create_table "customers", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
